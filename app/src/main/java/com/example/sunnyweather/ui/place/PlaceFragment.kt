6a71501af52074372f6c080c3fe655ee49fa1491
maichanghan <maichanghan@xiaomi.com>
2023-09-20 15:58:03 +0800

    加入切换城市和手动更新天气的功能

--- app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.kt
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sunnyweather.MainActivity
 import com.example.sunnyweather.databinding.FragmentPlaceBinding
 import com.example.sunnyweather.ui.weather.WeatherActivity
 
@@ -37,7 +38,7 @@ class PlaceFragment:Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         //重新加载页面时，自动打开上一次访问的城市天气信息
-        if(viewModel.isSaved()){
+        if (activity is MainActivity && viewModel.isSaved()) {
             val place=viewModel.getSavedPlace()
             val intent =Intent(context,WeatherActivity::class.java).apply {
                 putExtra("location_lng",place.location.lng)
--- app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
@@ -1,15 +1,19 @@
 package com.example.sunnyweather.ui.weather
 
+import android.content.Context
 import android.graphics.Color
 import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.WindowCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.sunnyweather.R
@@ -21,11 +25,10 @@ import java.util.Locale
 
 class WeatherActivity:AppCompatActivity() {
     val viewModel by lazy { ViewModelProvider(this).get(WeatherViewModel::class.java)}
-    private lateinit var binding: ActivityWeatherBinding
+    lateinit var binding: ActivityWeatherBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val decorView = window.decorView
 
         //沉浸状态栏
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -53,8 +56,34 @@ class WeatherActivity:AppCompatActivity() {
                 Toast.makeText(this,"无法获取天气信息",Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
+            binding.swipeRefresh.isRefreshing = false
         })
+
+        //下拉刷新
+        binding.swipeRefresh.setColorSchemeResources(R.color.colorPrimary)
+        refreshWeather()
+        binding.swipeRefresh.setOnRefreshListener {
+            refreshWeather()
+        }
+
+        //滑动菜单
+        binding.now.nav.setOnClickListener{
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+        binding.drawerLayout.addDrawerListener(object :DrawerLayout.DrawerListener{
+            override fun onDrawerStateChanged(newState: Int) { }
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) { }
+            override fun onDrawerOpened(drawerView: View) { }
+            override fun onDrawerClosed(drawerView: View) {
+                val manager=getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(drawerView.windowToken,InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        })
+    }
+
+    fun refreshWeather(){
         viewModel.refreshWeather(viewModel.lng,viewModel.lat)
+        binding.swipeRefresh.isRefreshing=true
     }
 
     private fun showWeatherInfo(weather: Weather) {
